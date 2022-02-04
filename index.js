6fb10a0420088a34fdafb67c0afdc46590b719ec
Ricardo Monterrosa <rickyantonio117@gmail.com>
2022-02-03 20:25:39 -0800

    add product details route

--- index.js
+++ index.js
@@ -4,10 +4,30 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const apiKey = 'c50ded683cf455d559363b00bb2ec63b';
+const baseUrl = `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scraper API.')
 });
+
+// GET product details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (err) {
+        res.json(err);
+        console.log(err);
+    }
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
